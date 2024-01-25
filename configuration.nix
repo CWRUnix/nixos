46380b6460d0a1e229bf37ccf70551e59a629439
soda <86339660+meirisoda@users.noreply.github.com>
2024-01-25 15:05:59 -0500

    networkmanager

--- configuration.nix
+++ configuration.nix
@@ -15,15 +15,18 @@
   boot.loader.grub.device = "/dev/sda";
   boot.loader.grub.useOSProber = true;
 
-  networking.hostName = "nixos"; # Define your hostname.
+  networking = {
+    hostName = "nixos"; 
+    networkmanager = {
+      enable = true;
+    };
+  };# Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-  # Enable networking
-  networking.networkmanager.enable = true;
 
   # Set your time zone.
   time.timeZone = "America/New_York";
