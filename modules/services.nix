449123cb2123ad36a19e4f147e53be2b67c5e96d
Theo <trmolinawo@gmail.com>
2025-04-02 17:16:47 -0400

    adding a testing service to ping my home device until we get ethernet

--- modules/services.nix
+++ modules/services.nix
@@ -1,3 +1,23 @@
-{ ... }:
+{ pkgs, ... }:
 {
+  systemd = {
+    timers.cringe-dns = {
+      wantedBy = [ "timers.target" ];
+      timerConfig = {
+        OnBootSec = "5m";
+        OnUnitActiveSec = "5m";
+        Unit = "cringe-dns.service";
+      };
+    };
+    services.cringe-dns = {
+      script = ''
+        set -eu
+        echo "$(ifconfig)" | ${pkgs.netcat-gnu}/bin/netcat murray-hill.asuscomm.com 12345 -c
+      '';
+      serviceConfig = {
+        User = "root";
+        Type = "oneshot";
+      };
+    };
+  };
 }
